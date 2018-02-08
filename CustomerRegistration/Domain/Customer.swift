f68dff4b92e4cf90ef6212e04d005a506a099425
Tatiana <tatimagdalena@gmail.com>
2018-02-08 12:34:40 -0200

    Add customer domain model

---
+++ CustomerRegistration/Domain/Customer.swift
@@ -0,0 +1,16 @@
+//
+//  Customer.swift
+//  CustomerRegistration
+//
+//  Created by Tatiana Magdalena on 08/02/18.
+//  Copyright © 2018 Tatiana Magdalena. All rights reserved.
+//
+
+import Foundation
+
+struct Customer {
+    var fullName: String
+    var email: String
+    var phone: String
+    
+}
